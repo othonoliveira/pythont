51b6807ead142c05f703684421bc32d22a3ec848
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-01 19:21:34 -0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,7 +1,12 @@
-print('Os números pares no intervalo de 1 a 50 são:')
-for c in range(0,51,2):
-    print(c,end=(''))
-    if c==50:
-        print('.',end=(''))
+from random import shuffle
+lista=[]
+for c in range(1,8):
+    n=float(input('Digite o peso da {}º pessoa: '.format(c)))
+    lista.append(n)
+c=0
+while c==0:
+    if lista[0]<lista[1]<lista[2]<lista[3]<lista[4]<lista[5]<lista[6]:
+        print('O maior peso foi de {}'.format(lista[6]),'\nO menor peso foi de {}'.format(lista[0]))
+        c=1
     else:
-        print(',',end=(''))
+        shuffle(lista)
