848103d3cb0b6a0c8af3cb6061293969522024ba
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-07 20:24:23 -0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,15 +1,12 @@
 l=[]
-for c in range(0,5):
-    l.append(int(input(f'Digite um número para a posição {c}: ')))
-print(f'Você digitou os valores: {l}')
-if l[0]==l[1]==l[2]==l[3]==l[4]:
-    print('Todos os valores são iguais!')
-else:
-    print(f'O maior valor digitado foi {max(l)} nas posições ',end='')
-    for c in range(0,5):
-        if max(l)==l[c]:
-            print(f'{c}...',end='')
-    print(f'\nO menor valor digitado foi {min(l)} nas posições ',end='')
-    for c in range(0,5):
-        if min(l)==l[c]:
-            print(f'{c}...',end='')
+c=0
+while True:
+    t=int(input('Digite um valor: '))
+    if t in l:
+        print(f'O valor {t} está duplicado! Não vou adicionar.')
+    else:
+        l.append(t)
+    r=str(input('Deseja continur? [S/N] ')).upper()
+    if r=='N':
+        break
+print(f'Os valores são {sorted(l)}')
