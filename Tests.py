a37f5a7b2bd1bde758be2795626e36390bdc29d9
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-06 22:00:31 -0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,6 +1,3 @@
-t=(int(input('Digite o 1º número: ')),int(input('Digite o 2º número: ')),int(input('Digite o 3º número: ')),int(input('Digite o 4º número: ')))
-print(f'O número 9 aparece {t.count(9)}')
-print(f'O primeiro falor 3 foi digitado na posição {t.index(3)+1}')
-for n in t:
-    if n%2==0:
-        print(f'Os números pares são:{n}',end=' ')
+t=('Leite', 10.00, 'Farinha', 5.00, 'Lápis', 1.50)
+print(f'Produtos | {t[0]} | {t[2]} | {t[4]}')
+print(f' Preços  | {t[1]}  | {t[3]}     | {t[5]}')
