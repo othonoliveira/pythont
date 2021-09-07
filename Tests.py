c07a4f0f838f959e9c874716d2f411097d4c98ba
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-07 19:38:25 -0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,3 +1,15 @@
-t=('Leite', 10.00, 'Farinha', 5.00, 'Lápis', 1.50)
-print(f'Produtos | {t[0]} | {t[2]} | {t[4]}')
-print(f' Preços  | {t[1]}  | {t[3]}     | {t[5]}')
+l=[]
+for c in range(0,5):
+    l.append(int(input(f'Digite um número para a posição {c}: ')))
+print(f'Você digitou os valores: {l}')
+if l[0]==l[1]==l[2]==l[3]==l[4]:
+    print('Todos os valores são iguais!')
+else:
+    print(f'O maior valor digitado foi {max(l)} nas posições ',end='')
+    for c in range(0,5):
+        if max(l)==l[c]:
+            print(f'{c}...',end='')
+    print(f'\nO menor valor digitado foi {min(l)} nas posições ',end='')
+    for c in range(0,5):
+        if min(l)==l[c]:
+            print(f'{c}...',end='')
