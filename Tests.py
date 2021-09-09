ff0564b4ec727658c1ac5d1d3c22b5c3a31cde25
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-09 19:25:56 -0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,30 +1,13 @@
+l=[]
 p=[]
-c=[]
-mai=men=0
-while True:
-    c.append(input('Digite o nome: '))
-    c.append(int(input('Digite o peso: ')))
-    if len(p)==0:
-        mai=men=c[1]
+i=[]
+for c in range(0,7):
+    r=int(input(f'Digite o {c+1}º valor: '))
+    if r%2==0:
+        p.append(r)
     else:
-        if c[1]>mai:
-            mai=c[1]
-        if c[1]<men:
-            men=c[1]
-    p.append(c[:])
-    c.clear()
-    r=str(input('Deseja adicionar outra pessoa? [S/N] ')).upper()
-    if r=='N':
-        break
-print(f'O número de inscrições foi de {len(p)}')
-if mai==men:
-    print('Os pesos são iguais!')
-else:
-    print(f'O maior peso foi de {mai}Kg, em ',end='')
-    for i in p:
-        if i[1]==mai:
-            print(f'[{i[0]}]',end='')
-    print(f'\nO o menor peso foi de {men}Kg, em ',end='')
-    for i in p:
-        if i[1]==men:
-            print(f'[{i[0]}]',end='')
+        i.append(r)
+l.append(p[:])
+l.append(i[:])
+print(f'Os números pares são {l[0]}')
+print(f'Os números impares são {l[1]}')
