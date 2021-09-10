ac0dd491cbd3f61672913f9325f2ffc3bec0f584
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-10 12:02:46 -0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,14 +1,21 @@
-from time import sleep
-from random import randint
-s=[]
-print('-'*30)
-print('Joga na MEGA SENA'.center(30,' '))
-print('-'*30)
-q=int(input('Quantos jogos quer que eu sortei? '))
-print(f'Soteando {q} jogos'.center(30,' '))
-for c in range(0,q):
-    for i in range(0,6):
-        s.append(randint(1,60))
-    sleep(1)
-    print(f'Jogo {c+1}: {sorted(s)}')
-    s.clear()
+a=[]
+l=[]
+while True:
+    l.append(str(input('Nome: ').capitalize()))
+    l.append(float(input('Nota 1: ')))
+    l.append(float(input('Nota 2: ')))
+    a.append(l[:])
+    t=str(input('Deseja continuar? [S/N] ')).upper()
+    if t=='N':
+        break
+    l.clear()
+print('Boletim'.center(30,'-'))
+for c in range(0,len(a)):
+    print(f'{c+1}.{a[c][0]} média {((a[c][1])+(a[c][2]))/2:.2f}')
+while True:
+    t=str(input('Deseja ver a nota de algum aluno? [S/N] ')).upper()
+    if t=='S':
+        t=int(input('Digite o número do aluno: '))
+        print(f'As notas de {a[t-1][0]} são n1:{a[t-1][1]:.2f} e n2:{a[t-1][2]:.2f}')
+    if t=='N':
+        break
