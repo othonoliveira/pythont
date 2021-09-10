2142e5e5a53ba8be1898425a50e57d244f00edb6
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-10 11:09:32 -0300

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,19 +1,14 @@
-l=[[],[],[]]
-s=mai=t=0
-for i in range(0,3):
-    for c in range(0,3):
-        l[c].append(int(input(f'Digite o valor para [{i},{c}]: ')))
-for i in range(0,3):
-    for c in range(0,3):
-        if l[i][c] % 2==0:
-            s+=l[i][c]
-        if c==2:
-            t+=l[i][c]
-        if i==1 and l[i][c]>mai:
-            mai=l[i][c]
-print('-='*30)
-print('',l[0],'\n',l[1],'\n',l[2],)
-print('-='*30)
-print(f'A soma dos valores pares é {s}')
-print(f'A soma dos valores da 3º coluna é {t}')
-print(f'O maior valor da 2º linha é {mai}')
+from time import sleep
+from random import randint
+s=[]
+print('-'*30)
+print('Joga na MEGA SENA'.center(30,' '))
+print('-'*30)
+q=int(input('Quantos jogos quer que eu sortei? '))
+print(f'Soteando {q} jogos'.center(30,' '))
+for c in range(0,q):
+    for i in range(0,6):
+        s.append(randint(1,60))
+    sleep(1)
+    print(f'Jogo {c+1}: {sorted(s)}')
+    s.clear()
