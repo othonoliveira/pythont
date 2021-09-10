2e7f29cee84da699ca7629b55a56059aab0357d3
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-10 15:49:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-a={}
-a['nome']=str(input('Digite o nome do aluno: '))
-a['media']=float(input('Digite a média do aluno: '))
-if 5<=a['media']<7:
-    a['situação']='Recuperação'
-if a['media']>=7:
-    a['situação']='Aprovado'
-if a['media']<5:
-    a['situação']='Reprovado'
-print(f'O aluno {a["nome"]} foi {a["situação"]}')
+from random import randint
+from operator import itemgetter
+from time import sleep
+j={'Jogador 1': int(randint(1,6)),'Jogador 2': int(randint(1,7)),'Jogador 3': int(randint(1,7)),'Jogador 4': int(randint(1,7))}
+r=dict()
+c=1
+for i,n in j.items():
+    print(f'O {i} tirou {n}.')
+    sleep(1)
+print('-'*30)
+r=sorted(j.items(), key=itemgetter(1), reverse=True)
+for i,n in r:
+    print(f'{c}º lugar: {i} com {n}')
+    sleep(1)
+    c+=1
