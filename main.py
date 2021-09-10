5bab565428032c89a6f13ace7de6260e833e3271
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-10 15:10:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-a=[]
-l=[]
-while True:
-    l.append(str(input('Nome: ').capitalize()))
-    l.append(float(input('Nota 1: ')))
-    l.append(float(input('Nota 2: ')))
-    a.append(l[:])
-    t=str(input('Deseja continuar? [S/N] ')).upper()
-    if t=='N':
-        break
-    l.clear()
-print('Boletim'.center(30,'-'))
-for c in range(0,len(a)):
-    print(f'{c+1}.{a[c][0]} média {((a[c][1])+(a[c][2]))/2:.2f}')
-while True:
-    t=str(input('Deseja ver a nota de algum aluno? [S/N] ')).upper()
-    if t=='S':
-        t=int(input('Digite o número do aluno: '))
-        print(f'As notas de {a[t-1][0]} são n1:{a[t-1][1]:.2f} e n2:{a[t-1][2]:.2f}')
-    if t=='N':
-        break
+a={}
+a['nome']=str(input('Digite o nome do aluno: '))
+a['media']=float(input('Digite a média do aluno: '))
+if 5<=a['media']<7:
+    a['situação']='Recuperação'
+if a['media']>=7:
+    a['situação']='Aprovado'
+if a['media']<5:
+    a['situação']='Reprovado'
+print(f'O aluno {a["nome"]} foi {a["situação"]}')
