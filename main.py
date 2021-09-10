597681cf1a98c5a4e0eb3ad8826c03c9f3fdfcbe
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-10 16:48:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from random import randint
-from operator import itemgetter
-from time import sleep
-j={'Jogador 1': int(randint(1,6)),'Jogador 2': int(randint(1,7)),'Jogador 3': int(randint(1,7)),'Jogador 4': int(randint(1,7))}
-r=dict()
-c=1
-for i,n in j.items():
-    print(f'O {i} tirou {n}.')
-    sleep(1)
-print('-'*30)
-r=sorted(j.items(), key=itemgetter(1), reverse=True)
-for i,n in r:
-    print(f'{c}º lugar: {i} com {n}')
-    sleep(1)
-    c+=1
+j={}
+gols=[]
+j['nome']=str(input('Qual o nome do jogador? '))
+j['qjogos']=int(input('Quantas pardidas ele jogou? '))
+for g in range(1,j['qjogos']+1):
+    gols.append(int(input(f'Quantos gols ele fez na {g}ª partida? ')))
+j['gols']=gols[:]
+print('-='*30)
+print(j)
+print('-='*30)
+for k,v in j.items():
+    print(f'O campo {k} tem valor {v}')
+print('-='*30)
+print(f'O jogador {j["nome"]} jogou {j["qjogos"]} partidas')
+for g in range(0,j['qjogos']):
+    print(f'Na partida {g+1}, fez {j["gols"][g]}')
