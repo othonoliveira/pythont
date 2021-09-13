5249b8fcac0921178726ff465a00883a3909e065
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-13 20:29:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def escreva(frase):
-    print('-'*len(frase))
-    print(frase)
-    print('-'*len(frase))
+from time import sleep
+def cont(init,fim,pas):
+    if init>fim:
+        con=init
+        while cont<=fim:
+            print(f'{con}', end=' ', flush=True)
+            con+=pas
+            sleep(0.5)
+        print('Fim!')
+    else:
+        con=init
+        while con<=fim:
+            print(f'{con}', end=' ', flush=True)
+            con-=pas
+            sleep(0.5)
+        print('Fim!')
 
 
-c=str(input('Digite uma frase: '))
-escreva(c)
+cont(-10,-8,-2)
