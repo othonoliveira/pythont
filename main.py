682c050164826a19b4efac97ff3ba6d972819145
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-13 18:58:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def area(a,b):
-    area=a*b
-    print(f'A área de um terreno com {a:.2f}x{b:.2f} é igual a {area:.2f}m²')
+def escreva(frase):
+    print('-'*len(frase))
+    print(frase)
+    print('-'*len(frase))
 
 
-l=float(input('Largura (m): '))
-c=float(input('Comprimento (m): '))
-area(l,c)
+c=str(input('Digite uma frase: '))
+escreva(c)
