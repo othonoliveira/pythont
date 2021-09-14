40b9f8d9fc6c631f9c074f33f7834d49d3d22d8c
Othon-Oliveira <othonoliveira08@gmail.com>
2021-09-13 22:53:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-def voto(a):
-    from datetime import datetime
-    r=datetime.now().year-a
-    return r
+def fat(a,b=bool()):
+    r=1
+    if a==1:
+        print(f'O reultado é: {a}')
+    elif b==False:
+        for c in range(1,a+1):
+            r*=c
+        print(f'O resultado é: {r}')
+    elif b==True:
+        for c in range(a,0,-1):
+            r*=c
+            if c!=1:
+                print(f'{c} X', end=' ')
+            if c==1:
+                print(f'1 = {r}')
 
 
-a=int(input('Digite o ano de nascimento: '))
-r=voto(a)
-if r>=18:
-    print('Voto OBRIGATÓRIO')
-if r<18 and r>=16:
-    print('Voto OPCIONAL')
-if r<16:
-    print('Voto NEGADO')
+f=int(input('Digite um número: '))
+fat(f, True)
